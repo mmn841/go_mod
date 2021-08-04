2b946707452dd4e0abe858073bc76bd92f68ab46
madhuran <madhuran@juniper.net>
2021-08-04 04:25:11 -0700

    v0.1.6, update ns_infra

--- ns_infra/hello.go
+++ ns_infra/hello.go
@@ -1,7 +1,7 @@
-package hello
+package ns_infra
 
 
 func Hello() string {
-    return "Hello World,v0.1.5"
+    return "Hello World,v0.1.6"
 }
 
--- ns_infra/hello_test.go
+++ ns_infra/hello_test.go
@@ -1,11 +1,11 @@
-package hello
+package ns_infra
 
 import (
     "testing"
 )
 
 func TestHello(t *testing.T) {
-    want := "Hello World,v0.1.5"
+    want := "Hello World,v0.1.6"
     if got := Hello(); got != want {
         t.Errorf("Hello() = %q, want %q", got, want)
     }
